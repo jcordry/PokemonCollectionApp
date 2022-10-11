35c636bb4bf84fc9de24b3e616e7b00c95612d54
u0026939 <j.cordry@tees.ac.uk>
2022-10-11 09:53:17 +0100

    Added a button

--- app/src/main/java/uk/ac/tees/mad/u0026939/pokemoncollectionapp/MainActivity.java
+++ app/src/main/java/uk/ac/tees/mad/u0026939/pokemoncollectionapp/MainActivity.java
@@ -3,12 +3,22 @@ package uk.ac.tees.mad.u0026939.pokemoncollectionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        Log.v("MainActivity", "Button clicked");
     }
 }
